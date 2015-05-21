22accfb98283efb6c003726f3cbfe720f143c5a5
Alpha Wang <awang@coupang.com>
2015-05-21 18:06:36 +0800

    [android] [2.1] First Activity

---
+++ app/src/main/java/com/alphawang/andoridfirstcode/FirstActivity.java
@@ -0,0 +1,16 @@
+package com.alphawang.andoridfirstcode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Alpha on May/21/15.
+ */
+public class FirstActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+    }
+
+}
