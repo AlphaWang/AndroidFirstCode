b16680cab7257d3ea7d1d083fea817161db4513d
Alpha Wang <awang@coupang.com>
2015-05-22 11:48:53 +0800

    [android] [2.2] Toast

--- app/src/main/java/com/alphawang/andoridfirstcode/FirstActivity.java
+++ app/src/main/java/com/alphawang/andoridfirstcode/FirstActivity.java
@@ -2,6 +2,9 @@ package com.alphawang.andoridfirstcode;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Alpha on May/21/15.
@@ -11,6 +14,19 @@ public class FirstActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+
+        buttonEvent();
+    }
+
+    private void buttonEvent() {
+        Button button = (Button) findViewById(R.id.button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "You clicked button!", Toast.LENGTH_LONG)
+                     .show();
+            }
+        });
     }
 
 }
